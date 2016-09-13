8cfc674fe6882cad0ec334e56e70294782d18fa3
Maghrisya Amalia Wardana <aamaliaw209@gmail.com>
2016-09-13 18:19:40 +0700

    First Commit Membuat Desain Tampilan Form Second Commit Membuat Coding dari setiap item

--- app/src/main/java/id/sch/smktelkom_mlg/learn/formpendaftaran/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/formpendaftaran/MainActivity.java
@@ -2,8 +2,21 @@ package id.sch.smktelkom_mlg.learn.formpendaftaran;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNL, etNP, etA;
+    TextView HasilNL, HasilNP, HasilAL, HasilJK, HasilAM, HasilJP;
+    Button buttonOK;
+    RadioGroup rgJK;
+    CheckBox cbjazz, cbpop, cbklasik, cbrock;
+    Spinner spJP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
